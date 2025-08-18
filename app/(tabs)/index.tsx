82a252c4d1969b8a4842cacfc59506e6a2fb315d
MciG-ggg <20949382132@qq.com>
2025-08-18 20:02:24 +0800

    feat: add picture display andd flatlist

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native';
+import ImageViewer from '@/components/ImageViewer';
+import { FlatList, StyleSheet, View } from 'react-native';
+
+const images = [
+  { id: '1', source: require('@/assets/images/nina.jpg') },
+  { id: '2', source: require('@/assets/images/nina2.jpg') },
+  // 可以继续添加更多图片...
+];
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <FlatList
+        data={images}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <ImageViewer imgSource={item.source} />
+        )}
+        contentContainerStyle={styles.listContent}
+        showsVerticalScrollIndicator={true}
+      />
     </View>
   );
 }
@@ -11,14 +26,12 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: '100%',
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#fff',
+  },
+  listContent: {
     padding: 10,
     gap: 10,
+    alignItems: 'center',
   },
 });
-
 
